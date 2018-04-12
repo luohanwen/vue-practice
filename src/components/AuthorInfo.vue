11c6da92425a087590b04b86f97c36c24612d854
haven <641136947@qq.com>
2018-04-12 14:22:56 +0800

    vue practice

---
+++ src/components/AuthorInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="authorInfo">
+    <div class="author">hello {{$route.params.name}}</div>
+    <router-link to="kebin">kebin</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthorInfo",
+  data() {
+    return {
+      author: {
+        name: "haven"
+      }
+    };
+  },
+  mounted() {
+    console.log("authorinfo begin");
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+@c-red: green;
+.author {
+  color: @c-red;
+  .px2px(font-size,12);
+}
+</style>
